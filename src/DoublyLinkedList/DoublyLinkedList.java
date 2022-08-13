e236bbabc53e3ff25fe472bc2313241c9a339cbd
Anish-Aby <anishaby963@gmail.com>
2022-08-13 13:50:18 +0530

    Add Doubly Linked List
    Added DoublyLinkedList package.

---
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,64 @@
+package DoublyLinkedList;
+
+public class DoublyLinkedList {
+    private Node header;
+    private Node trailer;
+    private int size = 0;
+
+    private Node createNode(int value){
+        Node newNode = new Node();
+        newNode.setValue(value);
+        return newNode;
+    }
+
+    public DoublyLinkedList(){
+        header = new Node(null, null);
+        trailer = new Node(null, header);
+        header.setNext(trailer);
+    }
+
+    public int getSize(){return size;}
+
+    public Boolean isEmpty(){
+        if (trailer.getPrev() == header)
+            return true;
+        else
+            return false;
+    }
+
+    public int peekFirst(){
+        int value = header.getNext().getValue();
+        return value;
+    }
+
+    public int peekLast(){
+        int value = trailer.getPrev().getValue();
+        return value;
+    }
+
+    public void addFirst(int value){
+        Node newNode = createNode(value);
+        newNode.setNext(header.getNext());
+        newNode.setPrev(header);
+        header.getNext().setPrev(newNode);
+        header.setNext(newNode);
+    }
+
+    public void addLast(int value){
+        Node newNode = createNode(value);
+        newNode.setNext(trailer);
+        newNode.setPrev(trailer.getPrev());
+        trailer.getNext().setNext(newNode);
+        trailer.setPrev(newNode);
+    }
+
+    public void removeFirst(){
+        header.getNext().getNext().setPrev(header);
+        header.setNext(header.getNext().getNext());
+    }
+
+    public void removeLast(){
+        trailer.getPrev().getPrev().setNext(trailer);
+        trailer.setPrev(trailer.getPrev().getPrev());
+    }
+}
---
+++ src/DoublyLinkedList/Node.java
@@ -0,0 +1,27 @@
+package DoublyLinkedList;
+
+public class Node {
+    private int value;
+    private Node next;
+    private Node prev;
+
+    public Node(){}
+    public Node(int value, Node next, Node prev){
+        this.value  = value;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public Node(Node next, Node prev){
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public void setValue(int value){this.value = value;}
+    public void setNext(Node next){this.next = next;}
+    public void setPrev(Node prev){this.prev = prev;}
+
+    public int getValue(){return value;}
+    public Node getNext(){return next;}
+    public Node getPrev(){return prev;}
+}
